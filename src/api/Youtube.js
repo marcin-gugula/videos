c96898a2f1ab71030afc95934f148d80ce3ab3d8
marcin.gugula <recble@gmail.com>
2020-09-16 22:02:02 +0200

    getting videos from api

--- src/api/Youtube.js
+++ src/api/Youtube.js
@@ -3,8 +3,9 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
-    part: "snippet",
-    maxResults: 5,
     key: process.env.REACT_APP_API_KEY,
+    part: "snippet",
+    type: "video",
+    maxResults: 5,
   },
 });
